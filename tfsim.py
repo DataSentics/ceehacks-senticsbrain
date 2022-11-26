d4abf13a9e1592d9ac2c69704c090af20e7f4f6f
michal.solc@datasentics.com <michal.solc@datasentics.com>
2022-11-26 07:22:58 +0000

    model

--- tfsim.py
+++ tfsim.py
@@ -16,9 +16,9 @@ from petastorm import TransformSpec
 # COMMAND ----------
 
 BATCH_SIZE = 32
-NUM_EPOCHS = 10
+NUM_EPOCHS = 15
 SHAPE = (1000,)
-TRAINING_SAMPLE_SIZE = 1000
+TRAINING_SAMPLE_SIZE = 300000
 
 # COMMAND ----------
 
@@ -29,6 +29,9 @@ tfsim.utils.tf_cap_memory()
 def get_model():
     inputs = tf.keras.layers.Input(shape=SHAPE)
     x = tf.keras.layers.experimental.preprocessing.Rescaling(1 / 255)(inputs)
+    x = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=500)
+    ])(x)
 #     x = tf.keras.layers.Conv2D(64, 3, activation="relu")(x)
 #     x = tf.keras.layers.Conv2D(32, 3, activation="relu")(x)
 #     x = tf.keras.layers.MaxPool2D()(x)
@@ -52,71 +55,31 @@ def get_compiled_model(lr=0.000005, distance = "cosine"):  # @param {type:"numbe
 
 # COMMAND ----------
 
-# df = pd.DataFrame(data={'ecg': [np.random.random((100)) for _ in range(10000)],
-#                         'labels': [np.random.randint(100) for _ in range(10000)]})
-
-# COMMAND ----------
-
-# dataset = tf.data.Dataset.from_tensor_slices((list(df['ecg'].values), df['labels'].values)).batch(32)
-
-# COMMAND ----------
-
-# dataset = tf.data.Dataset.from_tensor_slices((list(df['ecg'].values), df['labels'].values))
-
-# COMMAND ----------
-
-# test_dataset = dataset.take(1000).batch(32)
-# train_dataset = dataset.skip(1000).batch(32)
-
-# COMMAND ----------
-
-# EPOCHS = 10  # @param {type:"integer"}
-# history = model.fit(train_dataset, epochs=EPOCHS, validation_data=test_dataset)
-
-# COMMAND ----------
-
-# plt.plot(history.history["loss"])
-# plt.plot(history.history["val_loss"])
-# plt.legend(["loss", "val_loss"])
-# plt.title(f"Loss: {loss.name} - LR: {LR}")
-# plt.show()
-
-# COMMAND ----------
-
-# dataset = tf.data.Dataset.from_tensor_slices((list(df['ecg'].values), df['labels'].values)).batch(32)
-
-# COMMAND ----------
-
-# model.calibrate()
-
-# COMMAND ----------
-
-# model.lookup(dataset)
-
-# COMMAND ----------
-
-# dataset.unbatch().split()
-
-# COMMAND ----------
-
-
-
-
-# COMMAND ----------
-
 # MAGIC %md ### using Petastorm
 # MAGIC [source](https://docs.databricks.com/_static/notebooks/deep-learning/petastorm-spark-converter-tensorflow.html)
 
 # COMMAND ----------
 
 df = spark.read.table("ceehacks_ecg_samples")
-df_train, df_val = df.limit(TRAINING_SAMPLE_SIZE).randomSplit([0.9, 0.1], seed=42)
+df_train, df_val, df_test = df.limit(TRAINING_SAMPLE_SIZE).randomSplit([0.8, 0.1, 0.1], seed=42)
 
 spark.conf.set(SparkDatasetConverter.PARENT_CACHE_DIR_URL_CONF, "file:///dbfs/tmp/petastorm/cache")
 
 converter_train = make_spark_converter(df_train)
 converter_val = make_spark_converter(df_val)
 print(f"train: {len(converter_train)}, val: {len(converter_val)}")
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+df_train.groupby(col('id')).count().display()
 
 # COMMAND ----------
 
@@ -136,31 +99,129 @@ transform_spec_fn = TransformSpec(
 
 # COMMAND ----------
 
+import mlflow
+
+# COMMAND ----------
+
 def train_and_evaluate():
     model = get_compiled_model(lr=0.001)
-    with converter_train.make_tf_dataset(transform_spec=transform_spec_fn, batch_size=BATCH_SIZE) as train_dataset, \
-         converter_val.make_tf_dataset(transform_spec=transform_spec_fn, batch_size=BATCH_SIZE) as val_dataset:
-        
-        # tf.keras only accept tuples, not namedtuples
-        train_dataset = train_dataset.map(lambda x: (x.slice, x.id))
-        steps_per_epoch = len(converter_train) // BATCH_SIZE
+    with mlflow.start_run(run_name="similarity_model_training") as run:
+        with converter_train.make_tf_dataset(transform_spec=transform_spec_fn, batch_size=BATCH_SIZE) as train_dataset, \
+             converter_val.make_tf_dataset(transform_spec=transform_spec_fn, batch_size=BATCH_SIZE) as val_dataset:
 
-        val_dataset = val_dataset.map(lambda x: (x.slice, x.id))
-        validation_steps = max(1, len(converter_val) // BATCH_SIZE)
+            # tf.keras only accept tuples, not namedtuples
+            train_dataset = train_dataset.map(lambda x: (x.slice, x.id))
+            steps_per_epoch = len(converter_train) // BATCH_SIZE
 
-        print(f"steps_per_epoch: {steps_per_epoch}, validation_steps: {validation_steps}")
+            val_dataset = val_dataset.map(lambda x: (x.slice, x.id))
+            validation_steps = max(1, len(converter_val) // BATCH_SIZE)
 
-        hist = model.fit(train_dataset, 
-                         steps_per_epoch=steps_per_epoch,
-                         epochs=NUM_EPOCHS,
-                         validation_data=val_dataset,
-                         validation_steps=validation_steps,
-                         verbose=2)
+            print(f"steps_per_epoch: {steps_per_epoch}, validation_steps: {validation_steps}")
+        # with mlflow.start_run(run_name="similarity_model_training") as run:
+            hist = model.fit(train_dataset, 
+                             steps_per_epoch=steps_per_epoch,
+                             epochs=NUM_EPOCHS,
+                             validation_data=val_dataset,
+                             validation_steps=validation_steps,
+                             verbose=2)
+            mlflow.keras.log_model(
+                 model,
+              artifact_path="similarity_model",
+              registered_model_name="similarity_model"
+              )
 #     return hist.history['val_loss'][-1], hist.history['val_accuracy'][-1] 
     return hist.history
   
 history = train_and_evaluate()
 history
+
+# COMMAND ----------
+
+model_name = "similarity_model"
+stage = 'Production'
+
+inference_model = mlflow.keras.load_model(model_uri=f"models:/{model_name}/{stage}")
+
+# COMMAND ----------
+
+plt.plot(history["loss"])
+plt.plot(history["val_loss"])
+plt.legend(["loss", "val_loss"])
+# plt.title(f"Loss: {loss.name} - LR: {LR}")
+plt.show()
+
+# COMMAND ----------
+
+df_test = df_test.toPandas()
+df_train_pandas = df_train.limit(5000).toPandas()
+
+# COMMAND ----------
+
+rv = []
+for image_batch in np.array_split(df_train_pandas.slice, BATCH_SIZE):
+    images = np.vstack(image_batch)
+    dataset = tf.data.Dataset.from_tensor_slices(images).batch(BATCH_SIZE)
+    preds = inference_model.predict(dataset)
+    rv.extend(preds)
+
+# COMMAND ----------
+
+df_train_pandas['embeddings'] = rv
+
+# COMMAND ----------
+
+import sklearn
+
+# COMMAND ----------
+
+from sklearn.decomposition import PCA
+
+# COMMAND ----------
+
+df_train_pandas.embeddings
+
+# COMMAND ----------
+
+arr = [df_train_pandas.loc[i].embeddings.tolist() for i in range(len(df_train_pandas))]
+
+# COMMAND ----------
+
+pca = PCA(n_components=2)
+pca.fit(arr)
+
+# COMMAND ----------
+
+transformed = pca.transform(arr)
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+
+# COMMAND ----------
+
+X = [t[0] for t in transformed]
+Y = [t[1] for t in transformed]
+
+# COMMAND ----------
+
+df_train_pandas.groupby('id').count()
+
+# COMMAND ----------
+
+x_n = X[200:300] #+ X[2500:2600]
+y_n = Y[200:300] #+ Y[2500:2600]
+
+x_n_n = X[4000:4100]
+y_n_n = Y[4000:4100]
+
+x_n_n_n = X[4800:4900]
+y_n_n_n = Y[4800:4900]
+
+# COMMAND ----------
+
+plt.scatter(x_n, y_n)
+plt.scatter(x_n_n, y_n_n)
+plt.scatter(x_n_n_n, y_n_n_n)
 
 # COMMAND ----------
 
